373b12c6a879d87a0ed5bf6a41c3d9bd25a924d6
Notebook <Notebook@mail.ru>
2024-09-28 11:53:49 +0300

    init commit

---
+++ src/main/java/ru/totom/javapomodoro/JavaPomodoroApplication.java
@@ -0,0 +1,13 @@
+package ru.totom.javapomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPomodoroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPomodoroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/totom/javapomodoro/JavaPomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.totom.javapomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPomodoroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
